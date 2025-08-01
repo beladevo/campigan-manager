6b43b6e8dd1f03724da4582266a806dc9632a09b
Omri Beladev <omribeladevnew@gmail.com>
2025-08-01 22:55:02 +0300

    claude changes

--- nestjs-service/src/app.module.ts
+++ nestjs-service/src/app.module.ts
@@ -1,31 +1,66 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import * as Joi from "joi";
 import { CampaignModule } from "./campaign/campaign.module";
 import { Campaign } from "./campaign/entities/campaign.entity";
-import { ConfigModule } from "./config/config.module";
-import { ConfigService } from "./config/config.service";
-import { ClientsModule, Transport } from "@nestjs/microservices";
 
 @Module({
   imports: [
-    ConfigModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().default('postgres'),
+        POSTGRES_PORT: Joi.number().default(5432),
+        POSTGRES_USER: Joi.string().default('postgres'),
+        POSTGRES_PASSWORD: Joi.string().default('postgres'),
+        POSTGRES_DATABASE: Joi.string().default('solara'),
+        RABBITMQ_URL: Joi.string().default('amqp://rabbitmq:rabbitmq@rabbitmq:5672'),
+        RABBITMQ_PREFETCH_COUNT: Joi.number().default(100),
+        RABBITMQ_EXCHANGE: Joi.string().default('solara.campaigns'),
+      }),
+    }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
+      inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: "postgres",
-        host: configService.postgresHost,
-        port: configService.postgresPort,
-        username: configService.postgresUser,
-        password: configService.postgresPassword,
-        database: configService.postgresDatabase,
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DATABASE'),
         entities: [Campaign],
         synchronize: true,
       }),
-      inject: [ConfigService],
     }),
+    ClientsModule.registerAsync([
+      {
+        name: 'CAMPAIGN_SERVICE',
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get('RABBITMQ_URL')],
+            queue: 'campaign.result',
+            noAck: false,
+            prefetchCount: configService.get('RABBITMQ_PREFETCH_COUNT'),
+            queueOptions: {
+              durable: true,
+              arguments: {
+                'x-dead-letter-exchange': 'solara.campaigns.dlx',
+                'x-dead-letter-routing-key': 'campaign.result.failed',
+              },
+            },
+            socketOptions: {
+              heartbeatIntervalInSeconds: 60,
+              reconnectTimeInSeconds: 5,
+            },
+          },
+        }),
+      },
+    ]),
     CampaignModule,
   ],
 })
 export class AppModule {}
-
-// dead letter queue
--- nestjs-service/src/campaign/campaign.module.ts
+++ nestjs-service/src/campaign/campaign.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClientsModule } from '@nestjs/microservices';
 import { CampaignController } from './campaign.controller';
 import { CampaignService } from './campaign.service';
 import { Campaign } from './entities/campaign.entity';
 import { RabbitMQModule } from '../rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Campaign]), RabbitMQModule],
+  imports: [
+    TypeOrmModule.forFeature([Campaign]), 
+    RabbitMQModule,
+    ClientsModule,
+  ],
   controllers: [CampaignController, CampaignService],
   providers: [CampaignService],
 })
